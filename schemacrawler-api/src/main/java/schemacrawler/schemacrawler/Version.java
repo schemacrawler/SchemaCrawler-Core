c4d4d9ee76b8adac714ee788e1d9d0ec5494bf7e
Sualeh Fatehi <sualeh@hotmail.com>
2025-12-24 12:31:45 -0500

    v17.2.2

--- schemacrawler-api/src/main/java/schemacrawler/schemacrawler/Version.java
+++ schemacrawler-api/src/main/java/schemacrawler/schemacrawler/Version.java
@@ -21,7 +21,7 @@ public final class Version extends BaseProductVersion {
 
   private static final String ABOUT =
       """
-      SchemaCrawler 17.2.0
+      SchemaCrawler 17.2.2
       Database schema discovery and comprehension tool
       https://www.schemacrawler.com/
       Copyright (c) 2000-2026, Sualeh Fatehi <sualeh@hotmail.com>.
@@ -34,7 +34,7 @@ public final class Version extends BaseProductVersion {
       """
           .stripIndent();
 
-  private static final Version VERSION = new Version("SchemaCrawler", "17.2.0");
+  private static final Version VERSION = new Version("SchemaCrawler", "17.2.2");
 
   /**
    * Information about this product.
