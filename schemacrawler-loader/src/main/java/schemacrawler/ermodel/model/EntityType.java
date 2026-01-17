c9f04022d13188e5f5e29709a29f67e4c886c8d0
Sualeh Fatehi <sualeh@hotmail.com>
2026-01-17 18:30:28 -0500

    Add copyright notice

--- schemacrawler-loader/src/main/java/schemacrawler/ermodel/model/EntityType.java
+++ schemacrawler-loader/src/main/java/schemacrawler/ermodel/model/EntityType.java
@@ -1,3 +1,11 @@
+/*
+ * SchemaCrawler
+ * http://www.schemacrawler.com
+ * Copyright (c) 2000-2026, Sualeh Fatehi <sualeh@hotmail.com>.
+ * All rights reserved.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
 package schemacrawler.ermodel.model;
 
 /** Represents the entity type of a table. */
