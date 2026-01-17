e0eade8f8dc723cb0d1851afb574e406d8260c5a
Sualeh Fatehi <sualeh@hotmail.com>
2026-01-16 20:38:35 -0500

    Add additional tests for issue #40

--- schemacrawler-tools/src/test/java/schemacrawler/tools/databaseconnector/DatabaseConnectorTest.java
+++ schemacrawler-tools/src/test/java/schemacrawler/tools/databaseconnector/DatabaseConnectorTest.java
@@ -14,19 +14,28 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 
+import java.util.Map;
+import java.util.Properties;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
 import schemacrawler.test.utility.DisableLogging;
 import schemacrawler.test.utility.TestDatabaseConnector;
 import schemacrawler.tools.executable.commandline.PluginCommand;
+import us.fatehi.test.utility.TestConnection;
 import us.fatehi.utility.datasource.DatabaseConnectionSource;
 import us.fatehi.utility.datasource.MultiUseUserCredentials;
 
 @DisableLogging
 public class DatabaseConnectorTest {
 
+  /**
+   * NOTE: This test does not test production code, but rather test utility code. However, it covers
+   * basic logic in the database connector class.
+   */
   @Test
+  @DisplayName("Test for DatabaseConnector operations")
   public void databaseConnector() throws Exception {
 
     final DatabaseConnector databaseConnector = new TestDatabaseConnector();
@@ -34,13 +43,10 @@ public class DatabaseConnectorTest {
         databaseConnector.getDatabaseServerType().getDatabaseSystemIdentifier(), is("test-db"));
     assertThat(databaseConnector.getHelpCommand().getName(), is("server:test-db"));
 
-    DatabaseConnectionOptions connectionOptions;
-    DatabaseConnectionSource connectionSource;
-
-    connectionOptions =
+    final DatabaseConnectionOptions connectionOptions =
         new DatabaseServerHostConnectionOptions(
             "test-db", "some-host", 2121, "some-database", null);
-    connectionSource =
+    final DatabaseConnectionSource connectionSource =
         databaseConnector.newDatabaseConnectionSource(
             connectionOptions, new MultiUseUserCredentials());
 
@@ -54,24 +60,6 @@ public class DatabaseConnectorTest {
         databaseConnector.setSchemaCrawlerOptionsDefaults(schemaCrawlerOptions),
         is(not(nullValue())));
 
-    assertThat(databaseConnector.supportsUrl("jdbc:test-db:some-database"), is(true));
-  }
-
-  /**
-   * NOTE: This test does not test production code, but rather test utility code. However, it covers
-   * basic logic in the database connector class.
-   */
-  @Test
-  public void testDatabaseConnector() throws Exception {
-    final DatabaseConnector databaseConnector = new TestDatabaseConnector();
-
-    final PluginCommand helpCommand = databaseConnector.getHelpCommand();
-    assertThat(helpCommand, is(notNullValue()));
-    assertThat(helpCommand.getName(), is("server:test-db"));
-
-    assertThat(
-        databaseConnector.getDatabaseServerType().getDatabaseSystemIdentifier(), is("test-db"));
-
     assertThat(databaseConnector.supportsUrl("jdbc:test-db:somevalue"), is(true));
     assertThat(databaseConnector.supportsUrl("jdbc:newdb:somevalue"), is(false));
     assertThat(databaseConnector.supportsUrl(null), is(false));
@@ -80,6 +68,7 @@ public class DatabaseConnectorTest {
   }
 
   @Test
+  @DisplayName("Unknown DatabaseConnector")
   public void unknownDatabaseConnector() {
     final DatabaseConnector databaseConnector = UnknownDatabaseConnector.UNKNOWN;
 
@@ -95,5 +84,47 @@ public class DatabaseConnectorTest {
 
     assertThat(
         databaseConnector.toString(), is("Database connector for unknown database system type"));
+  }
+
+  @Test
+  @DisplayName("DatabaseConnector with known properties")
+  public void urlxKnown() throws Exception {
+    urlxTest(Map.of("key", "value"), false);
+  }
+
+  @Test
+  @DisplayName("DatabaseConnector with no additional properties")
+  public void urlxNull() throws Exception {
+    urlxTest(null, true);
+  }
+
+  @Test
+  @DisplayName("DatabaseConnector with unknown properties")
+  public void urlxUnknown() throws Exception {
+    urlxTest(Map.of("unknown-key", "value"), true);
+  }
+
+  private void urlxTest(final Map<String, String> connectionProperties, final boolean returnsEmpty)
+      throws Exception {
+
+    final DatabaseConnector databaseConnector = new TestDatabaseConnector();
+
+    final DatabaseConnectionOptions connectionOptions =
+        new DatabaseServerHostConnectionOptions(
+            "test-db", "some-host", 2121, "some-database", connectionProperties);
+    final DatabaseConnectionSource connectionSource =
+        databaseConnector.newDatabaseConnectionSource(
+            connectionOptions, new MultiUseUserCredentials());
+
+    final TestConnection connection = connectionSource.get().unwrap(TestConnection.class);
+    if (returnsEmpty) {
+      assertThat(connection.getConnectionProperties().size(), is(0));
+    } else {
+      final Properties expectedProperties = new Properties();
+      if (connectionProperties != null) {
+        expectedProperties.putAll(connectionProperties);
+      }
+      assertThat(connection.getConnectionProperties(), is(expectedProperties));
+    }
   }
 }
