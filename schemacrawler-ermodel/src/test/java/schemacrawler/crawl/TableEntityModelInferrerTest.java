0926d0ff703a5ba00a4f559292954f2a0d253562
Sualeh Fatehi <sualeh@hotmail.com>
2026-01-19 13:30:08 -0500

    Revert "Move inferrer into the implementation"
    This reverts commit cc22978b4f75eed56de8081a1309b280f109598f.

--- schemacrawler-ermodel/src/test/java/schemacrawler/crawl/TableEntityModelInferrerTest.java
+++ schemacrawler-ermodel/src/test/java/schemacrawler/crawl/TableEntityModelInferrerTest.java
@@ -12,8 +12,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.Test;
-
-import schemacrawler.ermodel.implementation.TableEntityModelInferrer;
+import schemacrawler.ermodel.build.TableEntityModelInferrer;
 import schemacrawler.ermodel.model.EntityType;
 import schemacrawler.ermodel.model.ForeignKeyCardinality;
 import schemacrawler.schemacrawler.SchemaReference;
