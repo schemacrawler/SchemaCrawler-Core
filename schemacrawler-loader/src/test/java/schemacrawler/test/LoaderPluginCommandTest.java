aa05a16260f6fc1ab090b7a881acfe9b5f4f2302
Sualeh Fatehi <sualeh@hotmail.com>
2026-03-27 20:52:23 -0400

    Add all loaders to plugin command test

--- schemacrawler-loader/src/test/java/schemacrawler/test/LoaderPluginCommandTest.java
+++ schemacrawler-loader/src/test/java/schemacrawler/test/LoaderPluginCommandTest.java
@@ -13,35 +13,63 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.jupiter.api.Test;
 import schemacrawler.tools.executable.commandline.PluginCommand;
+import schemacrawler.tools.loader.catalog.CatalogLoaderProvider;
 import schemacrawler.tools.loader.catalog.counts.TableRowCountsLoaderProvider;
+import schemacrawler.tools.loader.catalog.offline.OfflineCatalogLoaderProvider;
+import schemacrawler.tools.loader.ermodel.ERModelLoaderProvider;
 import schemacrawler.tools.loader.ermodel.attributes.AttributesLoaderProvider;
+import schemacrawler.tools.loader.ermodel.implicitassociations.ImplicitAssociationsLoaderProvider;
 
 public class LoaderPluginCommandTest {
 
   @Test
-  public void loaderPluginCommands() {
+  public void catalogLoaderPluginCommands() {
 
-    final PluginCommand rowCountsPluginCommand =
-        new TableRowCountsLoaderProvider().getCommandLineCommand();
-    assertThat(
-        rowCountsPluginCommand.toString(),
-        is(
-            """
-            PluginCommand[name='countsloader', options=[\
-            PluginCommandOption[name='load-row-counts', valueClass=java.lang.Boolean], \
-            PluginCommandOption[name='no-empty-tables', valueClass=java.lang.Boolean]\
-            ]]\
-            """));
+    catalogLoaderPluginCommandLine(
+        new TableRowCountsLoaderProvider(),
+        """
+        PluginCommand[name='countsloader', options=[\
+        PluginCommandOption[name='load-row-counts', valueClass=java.lang.Boolean], \
+        PluginCommandOption[name='no-empty-tables', valueClass=java.lang.Boolean]\
+        ]]\
+        """);
 
-    final PluginCommand attributesPluginCommand =
-        new AttributesLoaderProvider().getCommandLineCommand();
-    assertThat(
-        attributesPluginCommand.toString(),
-        is(
-            """
-            PluginCommand[name='attributesloader', options=[\
-            PluginCommandOption[name='attributes-file', valueClass=java.lang.String]\
-            ]]\
-            """));
+    catalogLoaderPluginCommandLine(
+        new OfflineCatalogLoaderProvider(),
+        """
+        PluginCommand[name='unknown', options=[]]\
+        """);
+  }
+
+  @Test
+  public void erModelLoaderPluginCommands() {
+
+    erModelLoaderPluginCommandLine(
+        new AttributesLoaderProvider(),
+        """
+        PluginCommand[name='attributesloader', options=[\
+        PluginCommandOption[name='attributes-file', valueClass=java.lang.String]\
+        ]]\
+        """);
+
+    erModelLoaderPluginCommandLine(
+        new ImplicitAssociationsLoaderProvider(),
+        """
+        PluginCommand[name='implicitassociationsloader', options=[\
+        PluginCommandOption[name='implicit-associations', valueClass=java.lang.Boolean], \
+        PluginCommandOption[name='infer-extension-tables', valueClass=java.lang.Boolean]]]\
+        """);
+  }
+
+  private void catalogLoaderPluginCommandLine(
+      final CatalogLoaderProvider loaderProvider, final String expectedToString) {
+    final PluginCommand pluginCommand = loaderProvider.getCommandLineCommand();
+    assertThat(pluginCommand.toString(), is(expectedToString));
+  }
+
+  private void erModelLoaderPluginCommandLine(
+      final ERModelLoaderProvider loaderProvider, final String expectedToString) {
+    final PluginCommand pluginCommand = loaderProvider.getCommandLineCommand();
+    assertThat(pluginCommand.toString(), is(expectedToString));
   }
 }
