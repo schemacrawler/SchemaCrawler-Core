f9bd2ed8bd65428dbd1a433caa3b16d8f5f998d2
Sualeh Fatehi <sualeh@hotmail.com>
2026-07-24 22:00:57 -0400

    Add snowflake test database scripts

---
+++ schemacrawler-testdb/src/main/resources/db/books/01_schemas_G.sql
@@ -0,0 +1,12 @@
+-- SchemaCrawler
+-- http://www.schemacrawler.com
+-- Copyright (c) 2000-2026, Sualeh Fatehi <sualeh@hotmail.com>.
+-- All rights reserved.
+-- SPDX-License-Identifier: EPL-2.0
+
+-- Snowflake syntax
+DROP DATABASE IF EXISTS BOOKS;
+CREATE DATABASE BOOKS;
+USE DATABASE BOOKS;
+CREATE SCHEMA BOOKS;
+USE SCHEMA BOOKS;
