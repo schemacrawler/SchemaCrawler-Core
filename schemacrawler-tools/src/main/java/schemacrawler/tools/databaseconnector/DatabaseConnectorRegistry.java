4b63f2ffb67e7ea1f44a8e0cd2fa06417db3c13f
Sualeh Fatehi <sualeh@hotmail.com>
2026-08-23 13:58:30 -0400

    Normalize MySQL and MariaDB support in one place

--- schemacrawler-tools/src/main/java/schemacrawler/tools/databaseconnector/DatabaseConnectorRegistry.java
+++ schemacrawler-tools/src/main/java/schemacrawler/tools/databaseconnector/DatabaseConnectorRegistry.java
@@ -10,6 +10,7 @@ package schemacrawler.tools.databaseconnector;
 
 import static java.util.Comparator.naturalOrder;
 import static schemacrawler.tools.databaseconnector.UnknownDatabaseConnector.UNKNOWN;
+import static us.fatehi.utility.Utility.isBlank;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -21,9 +22,6 @@ import java.util.ServiceLoader;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import static us.fatehi.utility.Utility.isBlank;
-
 import schemacrawler.schemacrawler.exceptions.InternalRuntimeException;
 import schemacrawler.tools.command.PluginCommandRegistry;
 import schemacrawler.tools.executable.commandline.PluginCommand;
@@ -67,11 +65,6 @@ public final class DatabaseConnectorRegistry extends BasePluginRegistry
             "Loading database connector, %s=%s",
             databaseSystemIdentifier, databaseConnector.getClass().getName()));
     databaseConnectorRegistry.put(databaseSystemIdentifier, databaseConnector);
-
-    // Special case: MariaDB is handled by the MySQL plugin
-    if ("mysql".equals(databaseSystemIdentifier)) {
-      databaseConnectorRegistry.put("mariadb", databaseConnector);
-    }
   }
 
   private static Map<String, DatabaseConnector> loadDatabaseConnectorRegistry() {
