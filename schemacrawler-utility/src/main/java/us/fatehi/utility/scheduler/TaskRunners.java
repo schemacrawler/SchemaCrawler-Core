17512c8efb173ae296b9a9ea820ccc5570cd2f37
Sualeh Fatehi <sualeh@hotmail.com>
2026-09-20 13:03:09 -0400

    Fix bug - now read environmental variables also; add javadocs (#324)
    * Use system config
    
    * Add further information on threading
    
    * Allow testability of 'TaskRunners' and improve javadocs
    
    * Rename test class
    
    * Add documentation in package-info

--- schemacrawler-utility/src/main/java/us/fatehi/utility/scheduler/TaskRunners.java
+++ schemacrawler-utility/src/main/java/us/fatehi/utility/scheduler/TaskRunners.java
@@ -10,40 +10,25 @@ package us.fatehi.utility.scheduler;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import us.fatehi.utility.readconfig.SystemPropertiesConfig;
+import us.fatehi.utility.readconfig.SystemConfig;
 
+/** Creates task runners for database loading. */
 public class TaskRunners {
 
   private static final Logger LOGGER = Logger.getLogger(TaskRunners.class.getName());
 
-  /**
-   * Set to {@code true} to run tasks on the main thread. This setting may be provided as an
-   * environment variable or a Java system property.
-   */
   private static final String SC_SINGLE_THREADED = "SC_SINGLE_THREADED";
 
-  /**
-   * Sets the maximum number of threads used to load the database catalog. This setting may be
-   * provided as an environment variable or a Java system property. The default is 5 threads, and
-   * the maximum is 10.
-   */
   private static final String SC_LOAD_MAX_THREADS = "SC_LOAD_MAX_THREADS";
 
   private static final int DEFAULT_LOAD_MAX_THREADS = 5;
 
-  /**
-   * Sets the per-batch task timeout in seconds. This setting may be provided as an environment
-   * variable or a Java system property. The default is 3600 seconds but a value of 0 or less
-   * disables the timeout.
-   */
   private static final String SC_LOAD_TIMEOUT_SECONDS = "SC_LOAD_TIMEOUT_SECONDS";
 
   private static final int DEFAULT_LOAD_TIMEOUT_SECONDS = 3600;
 
   public static TaskRunner getTaskRunner(final String id) {
-    final boolean isSingleThreaded =
-        new SystemPropertiesConfig().getBooleanValue(SC_SINGLE_THREADED);
-    if (isSingleThreaded) {
+    if (isSingleThreaded()) {
       LOGGER.log(Level.CONFIG, "Loading database schema in the main thread");
       return new MainThreadTaskRunner(id);
     }
@@ -52,12 +37,15 @@ public class TaskRunners {
     return new MultiThreadedTaskRunner(id, threadingOptions);
   }
 
-  private static ThreadingOptions getThreadingOptions() {
+  static ThreadingOptions getThreadingOptions() {
     final int maxThreads =
-        new SystemPropertiesConfig().getIntegerValue(SC_LOAD_MAX_THREADS, DEFAULT_LOAD_MAX_THREADS);
+        new SystemConfig().getIntegerValue(SC_LOAD_MAX_THREADS, DEFAULT_LOAD_MAX_THREADS);
     final int timeoutSeconds =
-        new SystemPropertiesConfig()
-            .getIntegerValue(SC_LOAD_TIMEOUT_SECONDS, DEFAULT_LOAD_TIMEOUT_SECONDS);
+        new SystemConfig().getIntegerValue(SC_LOAD_TIMEOUT_SECONDS, DEFAULT_LOAD_TIMEOUT_SECONDS);
     return new ThreadingOptions(maxThreads, timeoutSeconds);
+  }
+
+  static boolean isSingleThreaded() {
+    return new SystemConfig().getBooleanValue(SC_SINGLE_THREADED);
   }
 }
---
+++ schemacrawler-utility/src/test/java/us/fatehi/utility/scheduler/TaskRunnersConfigTest.java
@@ -0,0 +1,117 @@
+/*
+ * SchemaCrawler
+ * http://www.schemacrawler.com
+ * Copyright (c) 2000-2026, Sualeh Fatehi <sualeh@hotmail.com>.
+ * All rights reserved.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package us.fatehi.utility.scheduler;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class TaskRunnersConfigTest {
+
+  @Test
+  public void invalidThreadingSettingsUseDefaults() {
+    System.setProperty("SC_SINGLE_THREADED", "false");
+    System.setProperty("SC_LOAD_MAX_THREADS", "not-a-number");
+    System.setProperty("SC_LOAD_TIMEOUT_SECONDS", "not-a-number");
+
+    assertThat(TaskRunners.isSingleThreaded(), is(false));
+
+    final ThreadingOptions options = TaskRunners.getThreadingOptions();
+
+    assertThat(options.maxThreads(), is(5));
+    assertThat(options.timeoutSeconds(), is(3600));
+
+    assertTaskRunnerReturnsMultiThreaded();
+  }
+
+  @Test
+  public void negativeThreadingSettingsAreClamped() {
+    System.setProperty("SC_SINGLE_THREADED", "false");
+    System.setProperty("SC_LOAD_MAX_THREADS", "-1");
+    System.setProperty("SC_LOAD_TIMEOUT_SECONDS", "-1");
+
+    assertThat(TaskRunners.isSingleThreaded(), is(false));
+
+    final ThreadingOptions options = TaskRunners.getThreadingOptions();
+
+    assertThat(options.maxThreads(), is(1));
+    assertThat(options.timeoutSeconds(), is(0));
+
+    assertTaskRunnerReturnsMultiThreaded();
+  }
+
+  @Test
+  public void noSettings() {
+
+    assertThat(TaskRunners.isSingleThreaded(), is(false));
+
+    final ThreadingOptions options = TaskRunners.getThreadingOptions();
+    assertThat(options.maxThreads(), is(5));
+    assertThat(options.timeoutSeconds(), is(3600));
+
+    assertTaskRunnerReturnsMultiThreaded();
+  }
+
+  @Test
+  public void readsThreadingSettings() {
+    System.setProperty("SC_SINGLE_THREADED", "false");
+    System.setProperty("SC_LOAD_MAX_THREADS", "3");
+    System.setProperty("SC_LOAD_TIMEOUT_SECONDS", "42");
+
+    assertThat(TaskRunners.isSingleThreaded(), is(false));
+
+    final ThreadingOptions options = TaskRunners.getThreadingOptions();
+    assertThat(options.maxThreads(), is(3));
+    assertThat(options.timeoutSeconds(), is(42));
+
+    assertTaskRunnerReturnsMultiThreaded();
+  }
+
+  @Test
+  public void singleThreadingSettingsAreUsed() {
+    System.setProperty("SC_SINGLE_THREADED", "true");
+    System.setProperty("SC_LOAD_MAX_THREADS", "1");
+    System.setProperty("SC_LOAD_TIMEOUT_SECONDS", "10");
+
+    assertThat(TaskRunners.isSingleThreaded(), is(true));
+
+    final ThreadingOptions options = TaskRunners.getThreadingOptions();
+
+    assertThat(options.maxThreads(), is(1));
+    assertThat(options.timeoutSeconds(), is(10));
+
+    assertTaskRunnerReturnsSingleThreaded();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    System.clearProperty("SC_SINGLE_THREADED");
+    System.clearProperty("SC_LOAD_MAX_THREADS");
+    System.clearProperty("SC_LOAD_TIMEOUT_SECONDS");
+  }
+
+  private void assertTaskRunnerReturnsMultiThreaded() {
+    assertThat(TaskRunners.isSingleThreaded(), is(false));
+    final TaskRunner runner = TaskRunners.getTaskRunner("test-multi");
+    assertThat(runner, is(not(nullValue())));
+    assertThat(runner, instanceOf(MultiThreadedTaskRunner.class));
+  }
+
+  private void assertTaskRunnerReturnsSingleThreaded() {
+    assertThat(TaskRunners.isSingleThreaded(), is(true));
+    final TaskRunner runner = TaskRunners.getTaskRunner("test-main");
+    assertThat(runner, is(not(nullValue())));
+    assertThat(runner, instanceOf(MainThreadTaskRunner.class));
+  }
+}
